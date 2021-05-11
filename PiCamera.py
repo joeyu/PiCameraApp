71cba32dd0e699edb2ea7ee2e65027306d404228
Zhou Yu <nap.zhou.yu@gmail.com>
2021-05-11 20:34:51 +0800

    Enhanced config file.

--- PiCamera.py
+++ PiCamera.py
@@ -1,11 +1,16 @@
-from gpiozero import Button
-from picamera import PiCamera
-import time
-from datetime import datetime, timedelta
+import logging
 import io
 import json
+import time
+from datetime import datetime, timedelta
+from gpiozero import Button
+from picamera import PiCamera
 
-def load_config(config_file):
+logging.basicConfig(level=logging.DEBUG)
+
+config_file = "config.json"
+
+def load_config(config_file:str):
     """
     Args:
         config_file: file name.
@@ -18,22 +23,30 @@ def load_config(config_file):
     
     return config
 
+def save_config(config_file:str, config):
+    """
+    Save the config into a json file.
+    """
+    with open(config_file, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(config, ensure_ascii=False, indent=4))
+        logging.info(f"Wrote file '{fp}'.")    
+
 def shutter_pressed():
     """
     Callback function when shutter is pressed.
     """
     timestamp = datetime.today().strftime("%Y-%m-%d-%H-%M-%S")
-    camera.capture('/home/pi/Pictures/%s.jpg' % timestamp)
+    camera.capture(f"{config['picture_directory']}/{timestamp}.jpg")
     return
     
 button_shutter = Button(26)
 button_shutter.when_pressed = shutter_pressed
 
-config = load_config('config.json')
+config = load_config(config_file)
 
 with PiCamera() as camera:
     camera.resolution = config['camera']['resolution']
-    print("camera.resolution = %s" % (camera.resolution, ) )
+    logging.debug(f"camera.resolution = {camera.resolution!r}")
     camera.framerate = config['camera']['framerate']
     camera.start_preview()
     while True:
